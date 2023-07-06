b1381b5b1d8655f68df5df4e0a13ad4977cb5811
Henry Gilbert <77060624+henrygilbert22@users.noreply.github.com>
2023-07-05 21:12:26 -0500

    saving

--- dashboard.py
+++ dashboard.py
@@ -66,7 +66,7 @@ def start_summarize_runner(website_url: Optional[str]):
     thread_event.set()
     thread = Thread(target=summarize, args=(website_url, chatgpt_util, tokenizer, thread_event, shared_dict,), daemon=True)
     cntx_thread = add_script_run_ctx(thread)
-    cntx_thread.start()
+   # cntx_thread.start()
     st.session_state['exec_thread'] = cntx_thread
 
     
@@ -86,26 +86,27 @@ st.write(
 )
 
 
-if not thread_event.is_set():
 
-    col1, col2 = st.columns(2)
+col1, col2 = st.columns(2)
 
-    with col1:
-        ai_output_area = st.empty()
-        ai_output_area.text_area(label="AI Output", value=st.session_state['ai_output'], height=400)
+with col1:
+    ai_output_area = st.empty()
+    ai_output_area.text_area(label="AI Output", value=st.session_state['ai_output'], height=400)
 
-    with  col2:
-        
-        st.text_area(label="System Prompt", value=st.session_state['system_prompt'], height=200)
-        chat_response = st.text_input(label="Enter a website URL to summarize and get AI suggestions")
-        chat_response_button = st.button(
-                label="Send Response",
-                type="primary",
-                on_click=start_summarize_runner,
-                args=(chat_response,))
-        
-        progress_bar_slot = st.empty()
+with  col2:
     
+    st.text_area(label="System Prompt", value=st.session_state['system_prompt'], height=200)
+    chat_response = st.text_input(label="Enter a website URL to summarize and get AI suggestions")
+    chat_response_button = st.button(
+            label="Send Response",
+            type="primary",
+            on_click=start_summarize_runner,
+            args=(chat_response,))
+    
+    if st.session_state['exec_thread']:
+        st.session_state['exec_thread'].start()
+    progress_bar_slot = st.empty()
+
 
     
 while thread_event.is_set():    
