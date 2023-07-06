3ce32152ae68e10ce1efa6e1dff6c741156f03c1
Henry Gilbert <77060624+henrygilbert22@users.noreply.github.com>
2023-07-05 21:21:25 -0500

    saving

--- dashboard.py
+++ dashboard.py
@@ -91,17 +91,18 @@ col1, col2 = st.columns(2)
 
 with col1:
     ai_output_area = st.empty()
-    ai_output_area.text_area(label="AI Output", value=st.session_state['ai_output'], height=400)
+    ai_output_area.text_area(label="AI Output", value=st.session_state['ai_output'], height=400, key='ai_output_1')
 
 with  col2:
     
-    st.text_area(label="System Prompt", value=st.session_state['system_prompt'], height=200)
-    chat_response = st.text_input(label="Enter a website URL to summarize and get AI suggestions")
+    st.text_area(label="System Prompt", value=st.session_state['system_prompt'], height=200, key='system_prompt_input')
+    chat_response = st.text_input(label="Enter a website URL to summarize and get AI suggestions", key='text_input')
     chat_response_button = st.button(
             label="Send Response",
             type="primary",
             on_click=start_summarize_runner,
-            args=(chat_response,))
+            args=(chat_response,),
+            key='text_input_button')
     
     progress_bar_slot = st.empty()
 
