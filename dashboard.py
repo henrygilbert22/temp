59a0aa91f2de39cec223b81fc28adf990e0da9e0
Henry Gilbert <77060624+henrygilbert22@users.noreply.github.com>
2023-07-05 21:28:07 -0500

    saving

--- dashboard.py
+++ dashboard.py
@@ -88,36 +88,41 @@ def start_summarize_runner(website_url: Optional[str]):
     
     st.session_state['exec_thread'] = cntx_thread
 
-col1, col2 = st.columns(2)
 
-with col1:
-    ai_output_area = st.empty()
-    ai_output_area.text_area(label="AI Output", value=st.session_state['ai_output'], height=400, key='ai_output_1')
-
-with  col2:
+def main():
     
-    st.text_area(label="System Prompt", value=st.session_state['system_prompt'], height=200, key='system_prompt_input')
-    chat_response = st.text_input(label="Enter a website URL to summarize and get AI suggestions", key='text_input')
-    chat_response_button = st.button(
-            label="Send Response",
-            type="primary",
-            on_click=start_summarize_runner,
-            args=(chat_response,),
-            key='text_input_button')
-    
-    progress_bar_slot = st.empty()
+    col1, col2 = st.columns(2)
 
+    with col1:
+        ai_output_area = st.empty()
+        ai_output_area.text_area(label="AI Output", value=st.session_state['ai_output'], height=400, key='ai_output_1')
 
-
-if st.session_state['exec_thread'] is not None:
-    ai_output_area.empty()
-    
-    while thread_event.is_set():    
-        progress_bar_slot.progress(st.session_state['progress_num'], st.session_state['progress_text'])
-        time.sleep(1)
+    with  col2:
         
-    progress_bar_slot.progress(100, "Done!")
-    ai_output_area.text_area(label="AI Output", value=st.session_state['ai_output'], height=400, key='final_ai_output')
-    st.session_state['exec_thread'] = None
-    progress_bar_slot.empty()
+        st.text_area(label="System Prompt", value=st.session_state['system_prompt'], height=200, key='system_prompt_input')
+        chat_response = st.text_input(label="Enter a website URL to summarize and get AI suggestions", key='text_input')
+        chat_response_button = st.button(
+                label="Send Response",
+                type="primary",
+                on_click=start_summarize_runner,
+                args=(chat_response,),
+                key='text_input_button')
+        
+        progress_bar_slot = st.empty()
 
+
+
+    if st.session_state['exec_thread'] is not None:
+        ai_output_area.empty()
+        
+        while thread_event.is_set():    
+            progress_bar_slot.progress(st.session_state['progress_num'], st.session_state['progress_text'])
+            time.sleep(1)
+            
+        progress_bar_slot.progress(100, "Done!")
+        ai_output_area.text_area(label="AI Output", value=st.session_state['ai_output'], height=400, key='final_ai_output')
+        st.session_state['exec_thread'] = None
+        progress_bar_slot.empty()
+
+if __name__ == '__main__':
+    main()
